8ad0ce1e6bec96d906c1a8c4bfc6e03de9cc0a29
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2025-05-04 00:32:42 -0300

    feat: create player card

--- src/presentation/components/PlayerCard.tsx
+++ src/presentation/components/PlayerCard.tsx
@@ -1,36 +1,32 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, PressableProps, StyleSheet, Text, View } from "react-native";
 
 import { Colors } from "@/src/constants/Colors";
+import { Ranking } from "@/src/domain/entities";
 import { AvatarPlayer } from "./AvatarPlayer";
 import { Icon } from "./Icon";
 
-type PlayerCardProps = {
-
+type PlayerCardProps = PressableProps & {
+    player: Ranking;
 };
 
-export function PlayerCard({ }: PlayerCardProps) {
+export function PlayerCard({ player, ...othersProps }: PlayerCardProps) {
     return (
-        <View style={styles.container}>
-            <AvatarPlayer 
-                imageUri="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUdpgjJWcHi-bdDMsDmOO4Yrk9pBcDYQsO4Q&s"
-                size="xs"
-            />
+        <Pressable style={styles.container} {...othersProps}>
+            <AvatarPlayer imageUri={player.image} size="xs" featuredColor={Colors.primary[100]} />
 
             <View style={styles.content}>
                 <Text style={styles.playerName}>
-                    John Doe
+                    {player.name}
                 </Text>
                 <Text style={styles.playerPoints}>
-                    10 xp
+                    {`${player.points} XP`}
                 </Text>
             </View>
 
             <View style={{ flex: 1, alignItems: 'flex-end' }}>
-                <Pressable>
-                    <Icon name="chevron-forward" color={Colors.primary[600]} />
-                </Pressable>
+                <Icon name="chevron-forward" color={Colors.primary[600]} />
             </View>
-        </View>
+        </Pressable>
     );
 }
 
@@ -54,6 +50,7 @@ const styles = StyleSheet.create({
         color: Colors.white, 
         marginTop: 4, 
         fontSize: 14, 
-        fontFamily: 'Poppins_400Regular' 
+        fontFamily: 'Poppins_400Regular',
+        textTransform: 'uppercase',
     },
 });
