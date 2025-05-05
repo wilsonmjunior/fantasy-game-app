99c27b36b7a35cf299d9fb368538dfc17d4bac78
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2025-05-04 21:24:54 -0300

    fix: add api url

--- src/infrastructure/api/index.tsx
+++ src/infrastructure/api/index.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.104:4000/'
-    // baseURL: "https://foot-mock-api.vercel.app/"
+    // baseURL: 'http://192.168.0.104:4000/'
+    baseURL: "https://foot-mock-api.vercel.app/"
 });
 
 export { api };
