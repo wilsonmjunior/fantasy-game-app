ea0903fea34971bdb721d91aade43be8355df194
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2025-05-04 21:14:54 -0300

    feat: onboarding type

---
+++ src/@types/onboarding.d.ts
@@ -0,0 +1,17 @@
+declare module '@/src/app/onboarding/page1' {
+  import { FC } from 'react';
+  const OnboardingPage1: FC;
+  export default OnboardingPage1;
+}
+
+declare module '@/src/app/onboarding/page2' {
+  import { FC } from 'react';
+  const OnboardingPage2: FC;
+  export default OnboardingPage2;
+}
+
+declare module '@/src/app/onboarding/page3' {
+  import { FC } from 'react';
+  const OnboardingPage3: FC;
+  export default OnboardingPage3;
+} 
