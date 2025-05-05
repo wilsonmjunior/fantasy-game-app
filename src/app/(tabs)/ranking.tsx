6d800521299b1b8b03c008b91bd9767a129da810
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2025-05-04 21:17:01 -0300

    feat: ranking screen

---
+++ src/app/(tabs)/ranking.tsx
@@ -0,0 +1,68 @@
+import { FlatList, StyleSheet, View } from "react-native";
+
+import { Colors } from "@/src/constants/Colors";
+import { getStatusBarHeight } from "@/src/status-bar";
+import { Header, PlayerCard } from "@/src/presentation/components";
+import { RankingPlayer } from "@/src/presentation/components/screens/Ranking";
+import { useRanking } from "@/src/presentation/hooks";
+
+export default function RankingScreen() {
+    const { ranking, topRanking } = useRanking();
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Header title="Ranking" />
+            </View>
+
+            <FlatList 
+                data={ranking}
+                ListHeaderComponent={() => (
+                    <View style={styles.content}>
+                        {
+                            topRanking.map((top, index) => (
+                                <RankingPlayer
+                                    key={`${index}-${top.name}`}
+                                    avatarImage={top.image}
+                                    avatarSize={top.position === 1 ? "lg" : "md"}
+                                    name={top.name}
+                                    points={top.points} 
+                                />
+                            ))
+                        }
+                    </View>
+                )}
+                ListHeaderComponentStyle={styles.listHeader}
+                keyExtractor={(item, index) => `${index}-${item.name}`}
+                renderItem={({ item }) => (
+                    <PlayerCard player={item} />
+                )}
+                style={{ 
+                    paddingHorizontal: 20,
+                }}
+                showsVerticalScrollIndicator={false}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Colors.background,
+        paddingTop: getStatusBarHeight + 24,
+    },
+    header: {
+        paddingHorizontal: 20,
+        paddingBottom: 24,
+    },
+    listHeader: {
+        marginBottom: 24,
+    },
+    content: {
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        gap: 10,
+    },
+});
