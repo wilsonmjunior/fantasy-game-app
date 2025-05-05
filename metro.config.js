89545e53a7900a04cd7de4e935ab59df40789678
Wilson Matokanovic Junior <wilsonmjuniorx@gmail.com>
2025-05-04 21:27:44 -0300

    fix: metro

--- metro.config.js
+++ metro.config.js
@@ -8,6 +8,7 @@ module.exports = (() => {
   config.transformer = {
     ...transformer,
     babelTransformerPath: require.resolve("react-native-svg-transformer"),
+    // assetPlugins: ['expo-asset/tools/hashAssetFiles'],
   };
   config.resolver = {
     ...resolver,
